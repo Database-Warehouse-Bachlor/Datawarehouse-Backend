eb2cd843895b0bc07a529ca048e047ba292b8375
PetterDesktop <pettersen_97@hotmail.com>
2021-02-22 13:14:00 +0100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Datawarehouse_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
