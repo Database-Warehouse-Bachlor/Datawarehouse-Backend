801d2ed4e3fddac1acad4d74a823f52f71f22016
Sigurd <sigurd1994@hotmail.com>
2021-02-25 09:42:14 +0100

    filenames

--- App_Data/Program.cs
+++ App_Data/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Datawarehouse_Backend.App_Start;
 
 namespace Datawarehouse_Backend.App_Data
 {
