f55c92af18310b2c577a8562b838d560359ab9da
PetterDesktop <pettersen_97@hotmail.com>
2021-02-26 11:33:23 +0100

    Cohesion impovement for JWT
    moved osme code to another class

--- Controllers/JWTAuthenticationController.cs
+++ Controllers/JWTAuthenticationController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using Datawarehouse_Backend.Models;
+using Datawarehouse_Backend.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -26,61 +27,29 @@ namespace Datawarehouse_Backend.Controllers
      }
 
      [HttpPost]
-     public IActionResult Login(string orgNum, string pass)
+     public IActionResult login(string orgNum, string pass)
      {
         UserModel login = new UserModel();
+        JwtTokenGenerate jwtTokenGenerate = new JwtTokenGenerate();
         login.OrgNum = orgNum;
         login.Password = pass;
         IActionResult response = Unauthorized();
 
-        var user = AuthenticateUser(login);
+
+        var user = jwtTokenGenerate.authenticateUser(login);
 
         if (user != null)
         {
-            var tokenStr = GenerateJSONWebToken(user).ToString();
+            var tokenStr = jwtTokenGenerate.generateJSONWebToken(user,_config).ToString();
             response = Ok(tokenStr);
         }
         return response;
      }
 
-        private UserModel AuthenticateUser(UserModel login)
-     {
-         UserModel user = null;
-         if(login.OrgNum=="1234" && login.Password=="admin")
-         {
-             user=new UserModel { OrgNum="1234",EmailAdress="tenant@mail.com",Password="admin"};
-         }
-         return user;
-     }
-
-     private string GenerateJSONWebToken(UserModel userinfo)
-     {
-         var securityKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-         var credentials=new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256);
-
-         var claims = new[]
-         {
-             new Claim(JwtRegisteredClaimNames.Sub,userinfo.OrgNum),
-             new Claim(JwtRegisteredClaimNames.Email,userinfo.EmailAdress),
-             new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
-         };
-         
-         var token=new JwtSecurityToken(
-             issuer:_config["Jwt:Issuer"],
-             audience: _config["Jwt:Issuer"],
-             claims,
-             expires: DateTime.Now.AddMinutes(120),
-             signingCredentials: credentials
-         );
-
-         var encodeToken = new JwtSecurityTokenHandler().WriteToken(token);
-         return encodeToken;
-       
-     }
 
     [Authorize]
     [HttpPost("Post")]
-     public string Post()
+     public string post()
      {
          var identity = HttpContext.User.Identity as ClaimsIdentity;
          IList<Claim> claim = identity.Claims.ToList();
@@ -95,8 +64,6 @@ namespace Datawarehouse_Backend.Controllers
              return new string[] { "Value1","value2","value" };
          }
      
-
-
 
  }
 
