a199517e755c4ce74c807866266702a48f60f5db
PetterDesktop <pettersen_97@hotmail.com>
2021-03-01 12:12:40 +0100

    Customer class

---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Datawarehouse_Backend
+{
+
+    public class Customer {
+        public long customerId {get; set;}
+        public String customerName {get; set;}
+        public String address {get; set;}
+
+        public int zipcode {get; set;}
+        
+        //public long businessId {get; set;}
+
+        
+    }
+}
