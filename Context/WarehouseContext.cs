dee73799476ea36e3f80a0c83747f701edc255fc
Sverre Lofthus <sverrlof@stud.ntnu.no>
2021-03-03 14:01:24 +0100

    Added warehousecontext file

---
+++ Context/WarehouseContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Datawarehouse_Backend.Context {
+    public class WarehouseContext : DbContext {
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options) {
+    
+        }
+
+        // Define database tables
+        
+        
+    }
+}
