3d7e2cc9d48ae3b851e44afb51639bd1bb824d38
Petter <Pettersen_97@hotmail.com>
2021-03-05 15:11:06 +0100

    eployee list

--- Models/Tennant.cs
+++ Models/Tennant.cs
@@ -1,15 +1,20 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Datawarehouse_Backend
 {
     public class Tennant {
-        [Key]
+        [key]
          public long id { get; set; }
          public string tennantName { get; set; }
          public string address { get; set; }
          public string zipcode { get; set; }
          public string city { get; set; }
+
          public long businessId { get; set; }
+         
+         public List<Employee> employee { get; set; }
+         
     }
 }
