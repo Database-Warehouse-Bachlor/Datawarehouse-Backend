4a84034a79650796c257702843489b3895af6c5e
Sigurd <sigurd1994@hotmail.com>
2021-04-30 19:19:41 +0200

    Statistic views complete

---
+++ ViewModels/ClientStatisticsView.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Datawarehouse_Backend.ViewModels
+{
+    public class ClientStatisticsView
+    {
+        public int numberOfCustomers { get; set; }
+        public int numberOfSuppliers { get; set; }
+    }
+}
